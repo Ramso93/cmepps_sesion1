f2e6ce4dc6db7604815287913d19445d22ea2816
Ramso <Ramso@DESKTOP-F436ANM>
2022-10-19 16:26:34 +0200

    1

---
+++ src/cmepps_sesion1/Cmepps_sesion1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cmepps_sesion1;
+
+/**
+ *
+ * @author Ramso
+ */
+public class Cmepps_sesion1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
